a5c5f49bf2f35a81eb34077443a146a4db3da223
AdamSlamler <Dajon12346@gmail.com>
2026-05-22 17:45:16 -0700

    Implement voice runtime event subscriber

--- src/server/modules/voice/events/runtimeEventSubscriber.ts
+++ src/server/modules/voice/events/runtimeEventSubscriber.ts
@@ -1,1 +1,29 @@
-export const runtimeEventSubscriber = 'runtimeEventSubscriber';
+import type {
+  RuntimeEventBus,
+  RuntimeEventUnsubscribe,
+} from './runtimeEventBus.js';
+import type {
+  VoiceRuntimeEventHandler,
+  VoiceRuntimeEventType,
+} from './runtimeEventTypes.js';
+
+export class RuntimeEventSubscriber {
+  constructor(private readonly bus: RuntimeEventBus) {}
+
+  on(
+    type: VoiceRuntimeEventType,
+    handler: VoiceRuntimeEventHandler
+  ): RuntimeEventUnsubscribe {
+    return this.bus.subscribe(type, handler);
+  }
+
+  onAny(handler: VoiceRuntimeEventHandler): RuntimeEventUnsubscribe {
+    return this.bus.subscribe('*', handler);
+  }
+}
+
+export function createRuntimeEventSubscriber(
+  bus: RuntimeEventBus
+): RuntimeEventSubscriber {
+  return new RuntimeEventSubscriber(bus);
+}
